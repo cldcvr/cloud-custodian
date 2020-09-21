5d65841ee0cf0a6bd1998a69586b1917fc5f0fdf
Riyaz Ali <riyaz@cldcvr.com>
2020-09-21 14:09:23 +0530

    feat: add abstract base class to provide native interploation support

--- c7n/actions/__init__.py
+++ c7n/actions/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 
-from .core import Action, EventAction, BaseAction, ActionRegistry
+from .core import Action, EventAction, BaseAction, BaseVariableSupportAction, ActionRegistry
 from .autotag import AutoTagUser
 from .invoke import LambdaInvoke
 from .metric import PutMetric
